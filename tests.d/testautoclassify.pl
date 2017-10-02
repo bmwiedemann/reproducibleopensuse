25648eca6d6e396ab4ff4a44a0c42a4b245beed4
Bernhard M. Wiedemann <bwiedemann@suse.de>
2017-10-02 13:16:49 +0000

    autoclassify: handle build failures smarter
    so that we first find which tweak fails the build
    and only then continue the normal bisection with all remaining bits

--- tests.d/testautoclassify.pl
+++ tests.d/testautoclassify.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 use strict;
-use Test::More tests => 5;
+use Test::More tests => 8;
 $ENV{PATH}="tests.d/bin:$ENV{PATH}";
 
 sub test1($$)
@@ -15,4 +15,7 @@ test1("0bit", "0 0 0 0 0 0 0 0 0 0");
 test1("1bit", "0 0 1 0 0 0 0 0 0 0");
 test1("3bit", "1 0 1 0 0 0 0 1 0 0");
 test1("nonbuilding", "0 0 1 0 0 0 0 0 0 0");
+test1("nonbuilding2", "0 0 0 1 0 0 0 0 0 0");
+test1("neverbuilding", "1 1 0 0 0 0 0 1 1 1 98");
+test1("nonbuildingunreproducible", "1 1 1 1 0 1 1 1 1 1 99");
 test1("unreproducible", "1 1 1 1 1 1 1 1 1 1 99");
