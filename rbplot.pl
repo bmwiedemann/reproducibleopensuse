1fd1626889254973f79a27d37934354bcf36d63f
Bernhard M. Wiedemann <bwiedemann@suse.de>
2019-02-06 13:13:26 +0000

    rbplot: first working version
    parse reproducible.json to create summary of status of packages
    as CSV output

---
+++ rbplot.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -w
+use strict;
+use JSON::XS;
+use POSIX qw(strftime);
+
+sub processfile
+{
+    my $json = shift;
+    my %status;
+    foreach my $pkg (@$json) {
+        my $s = $pkg->{status};
+        $status{$s}++;
+    }
+    return \%status;
+}
+
+sub dumpstatus(@)
+{
+    my @headers=(qw(datum reproducible FTBR FTBFS other));
+    print join(",",@headers)."\n";
+    foreach my $line (@_) {
+        #foreach(sort(keys(%$line))) { print "$_ $line->{$_} " } # raw stat dump
+        foreach(qw(mtime reproducible unreproducible FTBFS other)) {
+            my $v = $line->{$_}||0;
+            if($_ eq "mtime") { $v = strftime("%Y-%m-%d", gmtime($v)) }
+            if($_ eq "other") { $v = ($line->{nobinaries}||0) + ($line->{waitdep}||0) } # ignoring notforus (other arch)
+            print "$v,"; # CSV out
+        }
+        print "\n";
+    }
+}
+
+my @files = @ARGV;
+my @status;
+
+foreach my $f (@files) {
+    open(my $fd, "<", $f) or die "error opening $f: $!";
+    my $json;
+    { local $/ ; $json = <$fd>; }
+    close $fd;
+    my $s = processfile(decode_json($json));
+    $s->{mtime} = (stat($f))[9];
+    push(@status, $s);
+}
+dumpstatus(@status);
